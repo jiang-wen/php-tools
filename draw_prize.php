a89879ceba6836f66c1ca3867ab98b61a387fe0e
JiangWen <947830008@qq.com>
2019-05-28 11:39:56 +0800

    添加注释

--- draw_prize.php
+++ draw_prize.php
@@ -11,7 +11,7 @@ $prize = array(
 );
 
 //模拟抽奖次数
-$time = 1000000;
+$time = 10000;
 
 for($i=0;$i<$time;$i++){
     $result = getResult($prize,$rate);
@@ -30,14 +30,22 @@ echo "</table>";
 
 //抽奖
 function getResult($prize,$rate){
+    //奖品数量数组
     $num_arr = array_column($prize,'num');
+    //将奖品数组按奖品数量排序
     array_multisort($num_arr,$prize);
+    //奖品总数量
     $total_num = array_sum($num_arr);
+    //抽奖区间总长度，每10人抽中一次 ， 那 就是  10 * 奖品总数量
     $prize_dump = $total_num*$rate;
+
+    //未中奖的，放到函数外面比较好
     $prize[] = array('id'=>0,'name'=>'谢谢参与','num'=>$prize_dump);
 
     foreach ($prize as $value){
+        //随机取数
         $rand = mt_rand(0,$prize_dump);
+
         if($rand < $value['num']){
             return $value;
         }else{
