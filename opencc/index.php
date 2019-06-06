18bf060730fbc2767918499a35bf527e7c08c88d
JiangWen <947830008@qq.com>
2019-06-06 14:43:55 +0800

    增加配置解释

--- opencc/index.php
+++ opencc/index.php
@@ -32,35 +32,16 @@
     </table>
 </form>
 
+<ul>
+    <li>s2t.json 简体到繁体</li>
+    <li>t2s.json 繁体到简体</li>
+    <li>s2tw.json 简体到台湾正体</li>
+    <li>tw2s.json 台湾正体到简体</li>
+    <li>s2hk.json 简体到香港繁体（香港小学学习字词表标准）</li>
+    <li>hk2s.json 香港繁体（香港小学学习字词表标准）到简体</li>
+    <li>s2twp.json 简体到繁体（台湾正体标准）并转换为台湾常用词汇</li>
+    <li>tw2sp.json 繁体（台湾正体标准）到简体并转换为中国大陆常用词汇</li>
+</ul>
 </body>
 </html>
 
-
-
-
-
-
-
-<?php
-
-
-//$opencc = "D:/opencc-1.0.4/bin/opencc ";
-//
-//$in_dir = "D:/phpStudy/WWW/market2.x7sy.com/application/controllers/";
-//$dir = scandir($in_dir);
-//$out_dir = "D:/market2_web_x7sy/";
-//
-//$config = "D:/opencc-1.0.4/share/opencc/s2t.json";
-//$out = "D:/test.txt";
-//@unlink($in_dir);
-//$arr = array_map(function($val) use($opencc,$in_dir,$out_dir,$config){
-//	if(!is_dir($in_dir.$val)){
-//
-//		return "opencc -i {$in_dir}{$val} -o {$out_dir}{$val} -c {$config} ";
-//	}
-//
-//},$dir);
-//
-//file_put_contents($out,implode("\r\n",$arr));
-//echo "执行完毕";
-?>
